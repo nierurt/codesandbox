06ee262afd3b36c9a742a9ea5c26f2a8d0fc1989
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-10 04:23:37 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+PORT=8080
 AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
-XRayConfig=https://raw.githubusercontent.com/bsefwe/glitch-Xray/main/etc/config.json
-# download execution
+
+
 chmod +x caddy xray
 
 # set caddy
@@ -14,10 +15,11 @@ wget $CADDYIndexPage -O usr/share/caddy/index.html && unzip -qo usr/share/caddy/
 
 
 # set config file
-
-wget -qO- $XRayConfig  | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
+cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
+cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
 
 
 # start service
 ./xray -config xray.json &
 ./caddy run --config etc/caddy/Caddyfile --adapter caddyfile
+
